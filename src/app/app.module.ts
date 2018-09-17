341c0850d2741c11b45fb26e78b30650ed9abd19
Thaís Fukuda <fukudathais@gmail.com>
2018-09-16 22:42:57 -0300

    2018-09-16 | Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModule, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppMaterialModule} from './material/app-material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { ModalComponent } from './layout/modal/modal.component';
+import { AboutComponent } from './layout/about/about.component';
+import { ModalAboutComponent } from './layout/modal-about/modal-about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
     HeaderComponent,
     FooterComponent,
     HomeComponent,
+    ModalComponent,
+    AboutComponent,
+    ModalAboutComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule,
     NgbModule.forRoot(),
     BrowserAnimationsModule,
     AppMaterialModule,
-    HttpClientModule
   ],
-  providers: [],
+  entryComponents: [
+    ModalAboutComponent
+  ],
+  providers: [NgbActiveModal],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { ModalComponent } from '../../layout/modal/modal.component';
+import { ModalAboutComponent } from '../../layout/modal-about/modal-about.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  providers: [NgbModal]
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
+
+  open() {
+    // const modalRef = this.modalService.open(ModalComponent);
+    const modalRef = this.modalService.open(ModalAboutComponent);
+    modalRef.componentInstance.title = 'About';
+  }
 
   ngOnInit() {
   }
